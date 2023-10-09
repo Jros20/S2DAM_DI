a0407d501bdaf286af8852af0c4449908ee8148f
juan antonio <juanantonioros20@gmail.com>
2023-10-09 10:02:43 +0200

    creacionCirculo

---
+++ Circulo.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using System.Drawing;
+
+namespace P2_2_Figuras;
+
+internal class Circulo : Figura
+    {
+    private Color color;
+    protected double _radio;
+
+    public Circulo(double radio , Color newcolor)
+        {
+            _radio = radio;
+             color=newcolor;
+        }
+
+    public Color Color { get => color; set => color = value; }
+    public double Radio  { get => _radio; set => _radio = value; }
+
+    public override double GetArea()
+        {
+            return Math.PI * Math.Pow(_radio, 2);
+        }
+
+            public override string ToString()
+        {
+            return $"""
+            ________________________________
+            FIGURA CIRCULO
+            -Radio: {_radio}
+            -Color :{color}
+            """;}
+}
