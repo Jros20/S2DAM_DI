cf5ff1f3317803717056e4db8ab28e79ff7f39c1
juan antonio <juanantonioros20@gmail.com>
2023-10-09 10:44:17 +0200

    CambiosFiguras

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -14,6 +14,24 @@ List<Figura> figuras = new List<Figura>();
   Circulo newCirculo = new Circulo(5.9, Color.Azure);
   TrianguloEquilatero newTriangulo = new TrianguloEquilatero(7, Color.Azure);
   Rectangulo newRectangulo= new Rectangulo(5.9,4.6, Color.Azure);
+  newCirculo.ToString();
+  newRectangulo.ToString();
+  newTriangulo.ToString();
+
+newRectangulo.Base = 15.0;
+newRectangulo.Altura = 8.0;
+newRectangulo.Color = Color.Green;
+
+
+newCirculo.Radio = 7.5; 
+newCirculo.Color = Color.Yellow;
+
+newTriangulo.Lado = 6.0; 
+newTriangulo.Color = Color.Blue; 
+
+  newCirculo.ToString();
+  newRectangulo.ToString();
+  newTriangulo.ToString();
   }
 
 }
